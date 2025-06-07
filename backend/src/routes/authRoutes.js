8c0ce5284d4eda472b60c6288ae6657b2a86b4a8
Pavel Buta <p.buta@student.tudelft.nl>
2025-06-07 20:50:57 +0200

    Fixed jwt renewal

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -61,8 +61,8 @@ router.post('/login', loginLimiter, async (req, res, next) => {
 
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
